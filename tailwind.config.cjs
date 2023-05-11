89d9277e971f8db569b000ef63cc5690aeeb3019
Charlie Coppinger <charles.coppinger@gmail.com>
2023-05-11 18:42:49 +1200

    add side-menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                glamour: ["Glamour", "serif"],
+                apercu: ["ApercuPro", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
