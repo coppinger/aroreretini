dcb518c0828cf6d4d8c845afbc943ef8040f25ca
Charlie Coppinger <charles.coppinger@gmail.com>
2023-05-26 14:36:26 +1200

    install astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            config: { applyBaseStyles: false },
-        }),
-    ],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image()]
 });
