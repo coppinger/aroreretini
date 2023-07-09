d5456d1ebca52f1fa01be009a75d800f37527bd5
Charlie Coppinger <charles.coppinger@gmail.com>
2023-07-10 09:42:23 +1200

    update astro config with site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+    site: "https://aroreretini.dev",
     integrations: [
         mdx(),
         sitemap(),
