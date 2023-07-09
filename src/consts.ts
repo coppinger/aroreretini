9bcbdccaebec05e1261f2b0f423bda08d0b26957
Charlie Coppinger <charles.coppinger@gmail.com>
2023-07-10 09:14:33 +1200

    add dynamic page titles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE: string = 'My personal website.';
-export const SITE_DESCRIPTION: string = 'Welcome to my website!';
+export const SITE_DESCRIPTION: string =
+    "Join me on my journey as an indie hacker. Here you can find my projects, essays, and collection of things that fuel my creativity.";
