a02f610360ed8785e2825021a013e53ddf7096e4
Charlie Coppinger <charles.coppinger@gmail.com>
2023-07-23 16:21:24 +1200

    dyrenh and codefork carried the team

---
+++ src/lib/menuItems.js
@@ -0,0 +1,27 @@
+const menu = [
+    ["./", "Index"],
+    // ["health", "Health", "Health stats from my Oura ring"],
+    ["about", "About", "A bunch of info about me"],
+    // ["divider", "Creations"],
+    ["contact", "Contact", "How to get ahold of me"],
+    ["projects", "Projects", "Past & present"],
+    ["essays", "Essays", "A collection of my writing"],
+    // ["updates", "Updates", "What’s going on in my life"],
+    // ["guides", "Guides", "Maps based on my experience"],
+    // ["divider", "Credits"],
+    ["people", "People", "Those whom I’m grateful to"],
+    // ["prophets", "Prophets", "Those who’ve shaped my beliefs"],
+    // ["divider", "Curation"],
+    ["quotes", "Quotes", "A list of my favorite quotes"],
+    // ["art", "Art", "A list of my favorite art"],
+    // ["music", "Music", "A list of my favorite music"],
+    // ["books", "Books", "A list of my favorite  books"],
+    // ["games", "Games", "A list of my favorite games"],
+    // ["tv", "TV", "A list of my favorite shows"],
+    // ["poems", "Poems", "A list of my favorite poems"],
+    // ["places", "Places", "A list of my favorite places"],
+];
+
+export default function menuItems() {
+    return menu;
+}
