25e8ab88e8c8db562a02e98547ff9f052b56ab27
Shivan Sivakumaran <me@shivan.xyz>
2024-10-17 23:21:27 +1300

    feat: adds rss feed for essays Closes #1

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,23 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+const essays = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string().optional(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+    tags: z.string().array().optional(),
+    content: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { essays };
